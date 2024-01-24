000c70fe6f7e64d40da02b698719560d6bbb72de
Saviour1001 <harshghodkar@gmail.com>
2024-01-24 23:24:27 +0530

    added the menu

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 mod check_balance;
 mod check_mina;
+use clap::Parser;
+
+// declare the parser
+
+#[derive(Parser)]
+struct commands {
+    subcmd: String,
+}
 
 #[tokio::main]
 async fn main() {
-    check_balance::check_balance().await;
+    // put an if statement here to check if the user has entered an argument
+    // if they have, then run the check_balance function
+    // if they haven't, then run the check_mina function
 
-    // check_mina::check_mina().await;
+    let subcmd = commands::parse().subcmd;
+
+    if subcmd == "check_balance" {
+        check_balance::check_balance().await;
+    } else if subcmd == "check_mina" {
+        check_mina::check_mina().await;
+    } else {
+        println!("Please enter a valid command");
+    }
 }

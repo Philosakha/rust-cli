93185f3d4dea16a15383596c4991e24a7adb538b
Saviour1001 <harshghodkar@gmail.com>
2024-02-13 21:48:18 +0530

    function completed

--- src/create_frontend.rs
+++ src/create_frontend.rs
@@ -1,3 +1,62 @@
 use clap::Parser;
 
-pub async fn create_frontend() {}
+use terminal_menu::{ button, label, menu, mut_menu, run };
+
+pub async fn create_frontend() {
+    info("Let's create a starting point for you");
+
+    let nextJSTemplate = "https://github.com/Philosakha/Starter-kit-mina-Auro-NEXTJS-13";
+    let reactTemplate = "https://github.com/Philosakha/starter-kit-mina-auro-react";
+
+    let menu = menu(
+        vec![
+            label("----------------------"),
+            label("terminal-menu"),
+            label("use wasd or arrow keys"),
+            label("enter to select"),
+            label("'q' or esc to exit"),
+            label("-----------------------"),
+            button("NextJS"),
+            button("React")
+        ]
+    );
+
+    run(&menu);
+
+    println!("Selected: {}", mut_menu(&menu).selected_item_name());
+
+    // ask for the name of the project
+    info("Please enter the name of your project");
+    let mut project_name = String::new();
+    std::io::stdin().read_line(&mut project_name).expect("Failed to read line");
+
+    if mut_menu(&menu).selected_item_name() == "NextJS" {
+        // clone the nextjs template with the project name
+
+        let cloneCommand = format!("git clone {} {}", nextJSTemplate, project_name);
+
+        std::process::Command
+            ::new("sh")
+            .arg("-c")
+            .arg(cloneCommand)
+            .output()
+            .expect("Failed to execute command");
+
+        info("NextJS project created successfully");
+    }
+
+    if mut_menu(&menu).selected_item_name() == "React" {
+        // clone the react template with the project name
+
+        let cloneCommand = format!("git clone {} {}", reactTemplate, project_name);
+
+        std::process::Command
+            ::new("sh")
+            .arg("-c")
+            .arg(cloneCommand)
+            .output()
+            .expect("Failed to execute command");
+
+        info("React project created successfully");
+    }
+}
